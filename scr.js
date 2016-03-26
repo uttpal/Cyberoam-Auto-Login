bbf9dfa2f349fdc908cbb31517dc555f54a0472e
Kumar Uttpal <uttpal25993@gmail.com>
2016-03-27 04:19:31 +0530

    Update scr.js

--- scr.js
+++ scr.js
@@ -1,21 +1,12 @@
 self.port.on("user", handleMessage);
 
 function handleMessage(message) {
-  
-
-
-
-document.getElementsByName('username')[0].value = message;
-
+    document.getElementsByName('username')[0].value = message;
 }
+
 self.port.on("pass", handle);
 
 function handle(pass) {
-  
-
-
-
-document.getElementsByName('password')[0].value = pass;
-document.getElementById('logincaption').click();
-
+    document.getElementsByName('password')[0].value = pass;
+    document.getElementById('logincaption').click();
 }
